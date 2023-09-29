7c2d252df50fda0179294497e3dc73dfe3a97a8f
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2023-09-29 19:33:58 +0700

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,1 +1,14 @@
+```ruby
+require 'telegram/bot'
 
+Telegram::Bot::Client.run('YOUR_TELEGRAM_BOT_TOKEN') do |bot|
+  bot.listen do |message|
+    case message
+    when Telegram::Bot::Types::Document
+      # Tambahkan logika pengiriman file ke channel seleksi dan pertanyaan kepada admin
+    when Telegram::Bot::Types::CallbackQuery
+      # Tambahkan logika penanganan jawaban dari admin di channel seleksi
+    end
+  end
+end
+```
